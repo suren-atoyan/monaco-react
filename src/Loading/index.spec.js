f8a014448441c613ea7680bd53ba5e835fa04261
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-19 14:08:45 +0400

    integrate jest and react-testing-library with test coverage

---
+++ src/Loading/index.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Loading from '.';
+import { render } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+
+describe('<Loading />', () => {
+  it('should check render with snapshot', () => {
+    const component = render(
+      <Loading />,
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should check is it wrapped with <div />', () => {
+    const component = render(
+      <Loading />,
+    );
+
+    const { container } = component;
+
+    expect(container.firstChild.tagName).toBe('DIV');
+  });
+
+  it('should check content', () => {
+    const content = 'Loading...';
+
+    const component = render(
+      <Loading content={content} />,
+    );
+
+    const { container } = component;
+
+    expect(container.textContent).toBe(content);
+  });
+});
