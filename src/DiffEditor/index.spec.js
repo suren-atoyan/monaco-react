d7a9c866ae37852912e5c9e58145d90978896f14
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-20 14:43:51 +0400

    add tests for DiffEditor

---
+++ src/DiffEditor/index.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DiffEditor from '.';
+import { render } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+
+describe('<DiffEditor />', () => {
+  it('should check render with snapshot', () => {
+    const component = render(
+      <DiffEditor />,
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
