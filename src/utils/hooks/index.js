2af539a28f98895bfd581b5d73af7ea9ffc73ae3
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-20 12:01:47 +0400

    --amend

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,5 +1,4 @@
 import useMount from './useMount';
 import useUpdate from './useUpdate';
-import useEditor from './useEditor';
 
-export { useMount, useUpdate, useEditor };
+export { useMount, useUpdate };
