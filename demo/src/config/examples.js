45e24d123352021beb3805509a349a7758d08fb6
Suren Atoyan <surenatoyan93@gmail.com>
2019-06-25 10:53:34 +0400

    change js example

--- demo/src/config/examples.js
+++ demo/src/config/examples.js
@@ -573,124 +573,56 @@ const examples = {
      }
   `),
   19: rTabs(`
-    import crypto from 'crypto';
-    import bcrypt from 'bcrypt';
-    import fs from './promisify-fs';
-    import path from 'path';
-    import config from '../config';
-    import env from './env';
-    import jwt from 'jsonwebtoken';
-    import db from '../db';
+    // @monaco-editor/react is Monaco editor wrapper for painless integration with React
+    // applications without need of webpack (or other module bundler)
+    // configuration files.
 
-    class Auth {
-      static genRandomCryptoString(len) {
-        // TODO ::: Replace crypto with tls.createSecureContext
-        return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
+    import React, { useState } from "react";
+    import ReactDOM from "react-dom";
+
+    import Editor from "@monaco-editor/react";
+    import examples from "./examples";
+
+    function App() {
+      const [theme, setTheme] = useState("light");
+      const [language, setLanguage] = useState("javascript");
+      const [isEditorReady, setIsEditorReady] = useState(false);
+
+      function handleEditorDidMount() {
+        setIsEditorReady(true);
       }
 
-      static async genSalt(saltRounds) {
-        return bcrypt.genSalt(saltRounds);
+      function toggleTheme() {
+        setTheme(theme === "light" ? "dark" : "light");
       }
 
-      static async hashPassword(password, salt) {
-        return bcrypt.hash(password, salt);
+      function toggleLanguage() {
+        setLanguage(language === "javascript" ? "python" : "javascript");
       }
 
-      async genPassword(password) {
-        const salt = await Auth.genSalt(config.get('default_salt_rounds'));
-        return Auth.hashPassword(password, salt);
-      }
+      return (
+        <>
+          <button onClick={toggleTheme} disabled={!isEditorReady}>
+            Toggle theme
+          </button>
+          <button onClick={toggleLanguage} disabled={!isEditorReady}>
+            Toggle language
+          </button>
 
-      async run() {
-        return this.checkAndGenAdminPasswordAndSecret();
-      }
-
-      async checkAndGenAdminPasswordAndSecret() {
-        const admin = await db.get('admin');
-        const secret = await db.get('secret');
-
-        if (!admin.password) {
-          const defaultPassword = config.get('default_admin_password');
-          const password = await this.genPassword(defaultPassword);
-          await db.set('admin.password', password);
-        }
-
-        if (!secret) {
-          const secret = Auth.genRandomCryptoString(10);
-          await db.set('secret', secret);
-        }
-      }
-
-      async sign(role) {
-        const token = jwt.sign(role, await db.get('secret'));
-
-        const activeTokens = await db.get('active_tokens');
-        if (!activeTokens.includes(token)) {
-          await db.insert('active_tokens', token);
-        }
-
-        return token;
-      }
-
-      async verify(token) {
-        const activeTokens = await db.get('active_tokens');
-        if (activeTokens.includes(token)) {
-          try {
-            return jwt.verify(token, await db.get('secret'));
-          } catch(err) {
-            await this.unsign(token);
-            return false;
-          }
-        }
-
-        return false;
-      }
-
-      async unsign(token) {
-        let activeTokens = await db.get('active_tokens');
-        activeTokens = activeTokens.filter(activeToken => activeToken !== token);
-        return db.set('active_tokens', activeTokens);
-      }
-
-      async checkPassword(password, hash) {
-        return bcrypt.compare(password, hash);
-      }
-
-      async getRole(name, pass) {
-        const teamInfo = await this.getTeamInfo(name, pass);
-
-        if (teamInfo) {
-          return {
-            isTeamMember: true,
-            team: teamInfo,
-          };
-        }
-
-        if (name === 'admin' && await this.isAdmin(pass)) {
-          return { isAdmin: true };
-        }
-
-        return null;
-      }
-
-      async getRoleByToken(token) {
-        return await this.verify(token);
-      }
-
-      async isAdmin(pass) {
-        const adminPass = await db.get('admin.password');
-
-        return await this.checkPassword(pass, adminPass);
-      }
-
-      async isGuest(token) {
-        const activeTokens = await db.get('active_tokens');
-
-        return activeTokens.includes(token);
-      }
+          <Editor
+            height="90vh" // By default, it fully fits with its parent
+            theme={theme}
+            language={language}
+            value={examples[language]}
+            editorDidMount={handleEditorDidMount}
+            loading={"Loading..."}
+          />
+        </>
+      );
     }
 
-    export default new Auth();
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(<App />, rootElement);
   `),
   20: rTabs(`
     {
