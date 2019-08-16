cfc551dc2847c39a26781f293ed72d0cad62cc0d
Suren Atoyan <surenatoyan93@gmail.com>
2019-08-16 21:16:52 +0400

    move out themes from main config file

--- src/config/index.js
+++ src/config/index.js
@@ -3,17 +3,6 @@ const config = {
     monacoLoader: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.17.1/min/vs/loader.js',
     monacoBase: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.17.1/min/vs'
   },
-
-  theme: {
-    'night-dark': {
-      base: 'vs-dark',
-      inherit: true,
-      rules: [],
-      colors: {
-        'editor.background': '#202124',
-      },
-    },
-  },
 }
 
 export default config;
