be28010ad3e51ccd33d9182f9376d6c8db8c6956
Suren Atoyan <contact@surenatoyan.com>
2019-10-09 00:55:42 +0400

    change the default version of monaco to 0.18.1

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   urls: {
-    monacoLoader: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.17.1/min/vs/loader.js',
-    monacoBase: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.17.1/min/vs'
+    monacoLoader: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.18.1/min/vs/loader.js',
+    monacoBase: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.18.1/min/vs'
   },
 }
 
