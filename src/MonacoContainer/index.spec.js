3114bd7dea7478f6a211d3e544010c9728ef1997
Suren Atoyan <contact@surenatoyan.com>
2019-11-09 20:33:06 +0400

    update snapshots

--- src/MonacoContainer/index.spec.js
+++ src/MonacoContainer/index.spec.js
@@ -6,7 +6,12 @@ import '@testing-library/jest-dom/extend-expect';
 describe('<MonacoContainer />', () => {
   it('should check render with snapshot', () => {
     const component = render(
-      <MonacoContainer />,
+      <MonacoContainer
+      	width="100%"
+      	height="100vh"
+      	loading="loading..."
+      	isEditorReady={false}
+      />,
     );
 
     expect(component).toMatchSnapshot();
