bc6ac17499cb1665f1bff897187a72af20f4cf49
Suren Atoyan <contact@surenatoyan.com>
2020-02-06 20:44:39 +0400

    update monaco version to 0.19.3 in default config file

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   urls: {
-    monacoLoader: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.0/min/vs/loader.js',
-    monacoBase: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.0/min/vs'
+    monacoLoader: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.3/min/vs/loader.js',
+    monacoBase: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.3/min/vs',
   },
 }
 
