0b47d9450cbbc1301654e6dc17783dc42fdf8f01
Suren Atoyan <contact@surenatoyan.com>
2021-01-07 18:33:31 +0400

    create  hook

---
+++ src/hooks/usePrevious/index.js
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+
+function usePrevious(value) {
+  const ref = useRef();
+  
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  
+  return ref.current;
+}
+
+export default usePrevious;
