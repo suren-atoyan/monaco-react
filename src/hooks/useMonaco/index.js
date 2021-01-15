c2fc280e9cdc84ac101025a5fd510f797a2726e2
Suren Atoyan <contact@surenatoyan.com>
2021-01-15 15:44:46 +0400

    create useMonaco hook

---
+++ src/hooks/useMonaco/index.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import loader from '@monaco-editor/loader';
+
+import useMount from '../useMount';
+
+function useMonaco() {
+  const [monaco, setMonaco] = useState(loader.__getMonacoInstance());
+
+  useMount(() => {
+    let cancelable;
+
+    if (!monaco) {
+      cancelable = loader.init();
+
+      cancelable.then(monaco => {
+        setMonaco(monaco);
+      });
+    }
+
+    return () => cancelable?.cancel();
+  });
+
+  return monaco;
+}
+
+export default useMonaco;
