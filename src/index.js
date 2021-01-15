c52fa21230d43a24caacef8f8bb69e9cca4ede21
Suren Atoyan <contact@surenatoyan.com>
2021-01-15 15:45:07 +0400

    export useMonaco from the entry file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import monaco from '@monaco-editor/loader';
 import DiffEditor from './DiffEditor';
 
 import ControlledEditor from './ControlledEditor';
+import useMonaco from './hooks/useMonaco';
 
-export { DiffEditor, ControlledEditor, monaco };
+export { DiffEditor, ControlledEditor, monaco, useMonaco };
 export { default } from './Editor';
