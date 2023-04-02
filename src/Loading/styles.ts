f5d7e377e36bde88bec6f52dae4a14637593b0df
suren-atoyan <contact@surenatoyan.com>
2023-04-02 21:04:26 +0400

    add prettier as a dev dep

---
+++ src/Loading/styles.ts
@@ -0,0 +1,13 @@
+import { type CSSProperties } from 'react';
+
+const styles: Record<string, CSSProperties> = {
+  container: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+
+export default styles;
