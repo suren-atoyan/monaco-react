f05dce8ebe4fd14dfb7b24441d925d8a8f996508
suren-atoyan <contact@surenatoyan.com>
2023-10-06 12:14:21 -0700

    use createRoot for bootstrapping

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './global.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
